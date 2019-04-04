14b015281ee918c0f868ff2709dd1297390e2f24
Oscar <oscarjavierho@gmail.com>
2019-04-04 06:26:53 -0430

    Final Proyect

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /*
+  internet mongo
+  mongoURI: 'mongodb+srv://Oscar:Oo12345!@mongodb-54ttx.mongodb.net/test?retryWrites=true'
+  */
+
+  //LOCAL MONGO
+  mongoURI: 'mongodb://localhost:27017/devconnector',
+  secretOrKey: 'secret'
+
+} 
